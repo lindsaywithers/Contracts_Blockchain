6691a5d298b898898836f4de8919e005f2c05344
garrettrowe <garrettrowe@yahoo.com>
2017-06-06 11:53:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -18,10 +18,13 @@ var cors = require('cors');
 var host = setup.SERVER.HOST;
 var port = setup.SERVER.PORT;
 
+var async = require('async');
+
 var Ibc1 = require('ibm-blockchain-js');														//rest based SDK for ibm blockchain
 var ibc = new Ibc1();
 var peers = null;
 var users = null;	
+var chaincode = null;	
 
 app.use(compression());
 app.use(morgan('dev'));
@@ -30,6 +33,275 @@ app.use(bodyParser.urlencoded());
 
 app.options('*', cors());
 app.use(cors());
+
+
+function cb_got_index(e, index){
+	if(e != null) console.log('[ws error] did not get marble index:', e);
+	else{
+		try{
+			var json = JSON.parse(index);
+			for(var i in json){
+				console.log('!', i, json[i]);
+				chaincode.query.read([json[i]], cb_got_marble);												//iter over each, read their values
+			}
+		}
+		catch(e){
+			console.log('[ws error] could not parse response', e);
+		}
+	}
+}
+
+
+function cb_got_marble(e, marble){
+	if(e != null) console.log('[ws error] did not get marble:', e);
+	else {
+		try{
+			return sendMsg({msg: 'marbles', marble: JSON.parse(marble)});
+		}
+		catch(e){}
+	}
+}
+
+function cb_invoked(e, a){
+	console.log('response: ', e, a);
+	return sendMsg(a);
+}
+
+function cb_chainstats(e, chain_stats){
+	if(chain_stats && chain_stats.height){
+		chain_stats.height = chain_stats.height - 1;								//its 1 higher than actual height
+		var list = [];
+		for(var i = chain_stats.height; i >= 1; i--){								//create a list of heights we need
+			list.push(i);
+			if(list.length >= 8) break;
+		}
+		list.reverse();																//flip it so order is correct in UI
+		async.eachLimit(list, 1, function(block_height, cb) {						//iter through each one, and send it
+			ibc.block_stats(block_height, function(e, stats){
+				if(e == null){
+					stats.height = block_height;
+					return sendMsg({msg: 'chainstats', e: e, chainstats: chain_stats, blockstats: stats});
+				}
+				cb(null);
+			});
+		}, function() {
+		});
+	}
+}
+
+function cb_got_trades(e, trades){
+	if(e != null) console.log('[ws error] did not get open trades:', e);
+	else {
+		try{
+			trades = JSON.parse(trades);
+			if(trades && trades.open_trades){
+				return sendMsg({msg: 'open_trades', open_trades: trades.open_trades});
+			}
+		}
+		catch(e){}
+	}
+}
+
+function sendMsg(json){
+	return JSON.stringify(json);
+}
+
+if(process.env.VCAP_SERVICES){																	//load from vcap, search for service, 1 of the 3 should be found...
+	var servicesObject = JSON.parse(process.env.VCAP_SERVICES);
+	for(var i in servicesObject){
+		if(i.indexOf('ibm-blockchain') >= 0){													//looks close enough
+			if(servicesObject[i][0].credentials && servicesObject[i][0].credentials.peers){		//found the blob, copy it to 'peers'
+				console.log('overwritting peers, loading from a vcap service: ', i);
+				peers = servicesObject[i][0].credentials.peers;
+				if(servicesObject[i][0].credentials.users){										//user field may or maynot exist, depends on if there is membership services or not for the network
+					console.log('overwritting users, loading from a vcap service: ', i);
+					users = servicesObject[i][0].credentials.users;
+				} 
+				else users = null;																//no security
+				break;
+			}
+		}
+	}
+}
+
+//filter for type1 users if we have any
+function prefer_type1_users(user_array){
+	var ret = [];
+	for(var i in users){
+		if(users[i].enrollId.indexOf('type1') >= 0) {	//gather the type1 users
+			ret.push(users[i]);
+		}
+	}
+
+	if(ret.length === 0) ret = user_array;				//if no users found, just use what we have
+	return ret;
+}
+
+//see if peer 0 wants tls or no tls
+function detect_tls_or_not(peer_array){
+	var tls = false;
+	if(peer_array[0] && peer_array[0].api_port_tls){
+		if(!isNaN(peer_array[0].api_port_tls)) tls = true;
+	}
+	return tls;
+}
+
+// ==================================
+// configure options for ibm-blockchain-js sdk
+// ==================================
+var options = 	{
+					network:{
+						peers: [peers[0]],																	//lets only use the first peer! since we really don't need any more than 1
+						users: prefer_type1_users(users),													//dump the whole thing, sdk will parse for a good one
+						options: {
+									quiet: true, 															//detailed debug messages on/off true/false
+									tls: detect_tls_or_not(peers), 											//should app to peer communication use tls?
+									maxRetry: 1																//how many times should we retry register before giving up
+								}
+					},
+					chaincode:{
+						zip_url: 'https://github.com/garrettrowe/Contracts_Blockchain/archive/master.zip',
+						unzip_dir: 'Contracts_Blockchain-master/chaincode',													//subdirectroy name of chaincode after unzipped
+						git_url: 'https://github.com/garrettrowe/Contracts_Blockchain/chaincode'
+					}
+				};
+
+
+// ---- Fire off SDK ---- //
+																	//sdk will populate this var in time, lets give it high scope by creating it here
+ibc.load(options, function (err, cc){														//parse/load chaincode, response has chaincode functions!
+	if(err != null){
+		console.log('! looks like an error loading the chaincode or network, app will fail\n', err);
+		if(!process.error) process.error = {type: 'load', msg: err.details};				//if it already exist, keep the last error
+	}
+	else{
+		chaincode = cc;
+		if(!cc.details.deployed_name || cc.details.deployed_name === ''){					//yes, go deploy
+			cc.deploy('init', ['99'], {delay_ms: 30000}, function(e){ 						//delay_ms is milliseconds to wait after deploy for conatiner to start, 50sec recommended
+				check_if_deployed(e, 1);
+			});
+		}
+		else{																				//no, already deployed
+			console.log('chaincode summary file indicates chaincode has been previously deployed');
+			check_if_deployed(null, 1);
+		}
+	}
+});
+
+//loop here, check if chaincode is up and running or not
+function check_if_deployed(e, attempt){
+	if(e){
+		cb_deployed(e);																		//looks like an error pass it along
+	}
+	else if(attempt >= 15){																	//tried many times, lets give up and pass an err msg
+		console.log('[preflight check]', attempt, ': failed too many times, giving up');
+		var msg = 'chaincode is taking an unusually long time to start. this sounds like a network error, check peer logs';
+		if(!process.error) process.error = {type: 'deploy', msg: msg};
+		cb_deployed(msg);
+	}
+	else{
+		console.log('[preflight check]', attempt, ': testing if chaincode is ready');
+		chaincode.query.read(['_marbleindex'], function(err, resp){
+			var cc_deployed = false;
+			try{
+				if(err == null){															//no errors is good, but can't trust that alone
+					if(resp === 'null') cc_deployed = true;									//looks alright, brand new, no marbles yet
+					else{
+						var json = JSON.parse(resp);
+						if(json.constructor === Array) cc_deployed = true;					//looks alright, we have marbles
+					}
+				}
+			}
+			catch(e){}																		//anything nasty goes here
+
+			// ---- Are We Ready? ---- //
+			if(!cc_deployed){
+				console.log('[preflight check]', attempt, ': failed, trying again');
+				setTimeout(function(){
+					check_if_deployed(null, ++attempt);										//no, try again later
+				}, 10000);
+			}
+			else{
+				console.log('[preflight check]', attempt, ': success');
+				cb_deployed(null);															//yes, lets go!
+			}
+		});
+	}
+}
+
+function cb_deployed(e){
+	if(e != null){
+		console.log('! looks like a deploy error, holding off on the starting the socket\n', e);
+		if(!process.error) process.error = {type: 'deploy', msg: e.details};
+	}
+	else{
+		ibc.monitor_blockheight(function(chain_stats){										//there is a new block, lets refresh everything that has a state
+			if(chain_stats && chain_stats.height){
+				console.log('hey new block, lets refresh and broadcast to all', chain_stats.height-1);
+				ibc.block_stats(chain_stats.height - 1, cb_blockstats);
+				//wss.broadcast({msg: 'reset'});
+				chaincode.query.read(['_marbleindex'], cb_got_index);
+				chaincode.query.read(['_opentrades'], cb_got_trades);
+			}
+			
+			//got the block's stats, lets send the statistics
+			function cb_blockstats(e, stats){
+				if(e != null) console.log('blockstats error:', e);
+				else {
+					chain_stats.height = chain_stats.height - 1;							//its 1 higher than actual height
+					stats.height = chain_stats.height;										//copy
+					//wss.broadcast({msg: 'chainstats', e: e, chainstats: chain_stats, blockstats: stats});
+				}
+			}
+
+			function cb_got_index(e, index){
+				if(e != null) console.log('marble index error:', e);
+				else{
+					try{
+						var json = JSON.parse(index);
+						for(var i in json){
+							console.log('!', i, json[i]);
+							chaincode.query.read([json[i]], cb_got_marble);					//iter over each, read their values
+						}
+					}
+					catch(e){
+						console.log('marbles index msg error:', e);
+					}
+				}
+			}
+			
+			//call back for getting a marble, lets send a message
+			function cb_got_marble(e, marble){
+				if(e != null) console.log('marble error:', e);
+				else {
+					try{
+						//wss.broadcast({msg: 'marbles', marble: JSON.parse(marble)});
+					}
+					catch(e){
+						console.log('marble msg error', e);
+					}
+				}
+			}
+			
+			//call back for getting open trades, lets send the trades
+			function cb_got_trades(e, trades){
+				if(e != null) console.log('trade error:', e);
+				else {
+					try{
+						trades = JSON.parse(trades);
+						if(trades && trades.open_trades){
+							//wss.broadcast({msg: 'open_trades', open_trades: trades.open_trades});
+						}
+					}
+					catch(e){
+						console.log('trade msg error', e);
+					}
+				}
+			}
+		});
+	}
+}
+
 
 app.use(function(req, res, next){
 	var keys;
@@ -135,76 +407,7 @@ app.use(function(err, req, res, next) {														// = development error hand
 		
 		*/
 
-	function cb_got_index(e, index){
-		if(e != null) console.log('[ws error] did not get marble index:', e);
-		else{
-			try{
-				var json = JSON.parse(index);
-				for(var i in json){
-					console.log('!', i, json[i]);
-					chaincode.query.read([json[i]], cb_got_marble);												//iter over each, read their values
-				}
-			}
-			catch(e){
-				console.log('[ws error] could not parse response', e);
-			}
-		}
-	}
 	
-
-	function cb_got_marble(e, marble){
-		if(e != null) console.log('[ws error] did not get marble:', e);
-		else {
-			try{
-				return sendMsg({msg: 'marbles', marble: JSON.parse(marble)});
-			}
-			catch(e){}
-		}
-	}
-	
-	function cb_invoked(e, a){
-		console.log('response: ', e, a);
-		return sendMsg(a);
-	}
-	
-	function cb_chainstats(e, chain_stats){
-		if(chain_stats && chain_stats.height){
-			chain_stats.height = chain_stats.height - 1;								//its 1 higher than actual height
-			var list = [];
-			for(var i = chain_stats.height; i >= 1; i--){								//create a list of heights we need
-				list.push(i);
-				if(list.length >= 8) break;
-			}
-			list.reverse();																//flip it so order is correct in UI
-			async.eachLimit(list, 1, function(block_height, cb) {						//iter through each one, and send it
-				ibc.block_stats(block_height, function(e, stats){
-					if(e == null){
-						stats.height = block_height;
-						return sendMsg({msg: 'chainstats', e: e, chainstats: chain_stats, blockstats: stats});
-					}
-					cb(null);
-				});
-			}, function() {
-			});
-		}
-	}
-	
-	function cb_got_trades(e, trades){
-		if(e != null) console.log('[ws error] did not get open trades:', e);
-		else {
-			try{
-				trades = JSON.parse(trades);
-				if(trades && trades.open_trades){
-					return sendMsg({msg: 'open_trades', open_trades: trades.open_trades});
-				}
-			}
-			catch(e){}
-		}
-	}
-
-	function sendMsg(json){
-		return JSON.stringify(json);
-	}
 
 
 
@@ -219,208 +422,4 @@ console.log('------------------------------------------ Server Up - ' + host + '
 
 
 
-if(process.env.VCAP_SERVICES){																	//load from vcap, search for service, 1 of the 3 should be found...
-	var servicesObject = JSON.parse(process.env.VCAP_SERVICES);
-	for(var i in servicesObject){
-		if(i.indexOf('ibm-blockchain') >= 0){													//looks close enough
-			if(servicesObject[i][0].credentials && servicesObject[i][0].credentials.peers){		//found the blob, copy it to 'peers'
-				console.log('overwritting peers, loading from a vcap service: ', i);
-				peers = servicesObject[i][0].credentials.peers;
-				if(servicesObject[i][0].credentials.users){										//user field may or maynot exist, depends on if there is membership services or not for the network
-					console.log('overwritting users, loading from a vcap service: ', i);
-					users = servicesObject[i][0].credentials.users;
-				} 
-				else users = null;																//no security
-				break;
-			}
-		}
-	}
-}
 
-//filter for type1 users if we have any
-function prefer_type1_users(user_array){
-	var ret = [];
-	for(var i in users){
-		if(users[i].enrollId.indexOf('type1') >= 0) {	//gather the type1 users
-			ret.push(users[i]);
-		}
-	}
-
-	if(ret.length === 0) ret = user_array;				//if no users found, just use what we have
-	return ret;
-}
-
-//see if peer 0 wants tls or no tls
-function detect_tls_or_not(peer_array){
-	var tls = false;
-	if(peer_array[0] && peer_array[0].api_port_tls){
-		if(!isNaN(peer_array[0].api_port_tls)) tls = true;
-	}
-	return tls;
-}
-
-// ==================================
-// configure options for ibm-blockchain-js sdk
-// ==================================
-var options = 	{
-					network:{
-						peers: [peers[0]],																	//lets only use the first peer! since we really don't need any more than 1
-						users: prefer_type1_users(users),													//dump the whole thing, sdk will parse for a good one
-						options: {
-									quiet: true, 															//detailed debug messages on/off true/false
-									tls: detect_tls_or_not(peers), 											//should app to peer communication use tls?
-									maxRetry: 1																//how many times should we retry register before giving up
-								}
-					},
-					chaincode:{
-						zip_url: 'https://github.com/garrettrowe/Contracts_Blockchain/archive/master.zip',
-						unzip_dir: 'Contracts_Blockchain-master/chaincode',													//subdirectroy name of chaincode after unzipped
-						git_url: 'https://github.com/garrettrowe/Contracts_Blockchain/chaincode'
-					}
-				};
-
-
-// ---- Fire off SDK ---- //
-var chaincode = null;																		//sdk will populate this var in time, lets give it high scope by creating it here
-ibc.load(options, function (err, cc){														//parse/load chaincode, response has chaincode functions!
-	if(err != null){
-		console.log('! looks like an error loading the chaincode or network, app will fail\n', err);
-		if(!process.error) process.error = {type: 'load', msg: err.details};				//if it already exist, keep the last error
-	}
-	else{
-		chaincode = cc;
-		if(!cc.details.deployed_name || cc.details.deployed_name === ''){					//yes, go deploy
-			cc.deploy('init', ['99'], {delay_ms: 30000}, function(e){ 						//delay_ms is milliseconds to wait after deploy for conatiner to start, 50sec recommended
-				check_if_deployed(e, 1);
-			});
-		}
-		else{																				//no, already deployed
-			console.log('chaincode summary file indicates chaincode has been previously deployed');
-			check_if_deployed(null, 1);
-		}
-	}
-});
-
-//loop here, check if chaincode is up and running or not
-function check_if_deployed(e, attempt){
-	if(e){
-		cb_deployed(e);																		//looks like an error pass it along
-	}
-	else if(attempt >= 15){																	//tried many times, lets give up and pass an err msg
-		console.log('[preflight check]', attempt, ': failed too many times, giving up');
-		var msg = 'chaincode is taking an unusually long time to start. this sounds like a network error, check peer logs';
-		if(!process.error) process.error = {type: 'deploy', msg: msg};
-		cb_deployed(msg);
-	}
-	else{
-		console.log('[preflight check]', attempt, ': testing if chaincode is ready');
-		chaincode.query.read(['_marbleindex'], function(err, resp){
-			var cc_deployed = false;
-			try{
-				if(err == null){															//no errors is good, but can't trust that alone
-					if(resp === 'null') cc_deployed = true;									//looks alright, brand new, no marbles yet
-					else{
-						var json = JSON.parse(resp);
-						if(json.constructor === Array) cc_deployed = true;					//looks alright, we have marbles
-					}
-				}
-			}
-			catch(e){}																		//anything nasty goes here
-
-			// ---- Are We Ready? ---- //
-			if(!cc_deployed){
-				console.log('[preflight check]', attempt, ': failed, trying again');
-				setTimeout(function(){
-					check_if_deployed(null, ++attempt);										//no, try again later
-				}, 10000);
-			}
-			else{
-				console.log('[preflight check]', attempt, ': success');
-				cb_deployed(null);															//yes, lets go!
-			}
-		});
-	}
-}
-
-// ============================================================================================================================
-// 												WebSocket Communication Madness
-// ============================================================================================================================
-function cb_deployed(e){
-	if(e != null){
-		console.log('! looks like a deploy error, holding off on the starting the socket\n', e);
-		if(!process.error) process.error = {type: 'deploy', msg: e.details};
-	}
-	else{
-		console.log('------------------------------------------ Websocket Up ------------------------------------------');
-
-		
-		// ========================================================
-		// Monitor the height of the blockchain
-		// ========================================================
-		ibc.monitor_blockheight(function(chain_stats){										//there is a new block, lets refresh everything that has a state
-			if(chain_stats && chain_stats.height){
-				console.log('hey new block, lets refresh and broadcast to all', chain_stats.height-1);
-				ibc.block_stats(chain_stats.height - 1, cb_blockstats);
-				//wss.broadcast({msg: 'reset'});
-				chaincode.query.read(['_marbleindex'], cb_got_index);
-				chaincode.query.read(['_opentrades'], cb_got_trades);
-			}
-			
-			//got the block's stats, lets send the statistics
-			function cb_blockstats(e, stats){
-				if(e != null) console.log('blockstats error:', e);
-				else {
-					chain_stats.height = chain_stats.height - 1;							//its 1 higher than actual height
-					stats.height = chain_stats.height;										//copy
-					//wss.broadcast({msg: 'chainstats', e: e, chainstats: chain_stats, blockstats: stats});
-				}
-			}
-			
-			//got the marble index, lets get each marble
-			function cb_got_index(e, index){
-				if(e != null) console.log('marble index error:', e);
-				else{
-					try{
-						var json = JSON.parse(index);
-						for(var i in json){
-							console.log('!', i, json[i]);
-							chaincode.query.read([json[i]], cb_got_marble);					//iter over each, read their values
-						}
-					}
-					catch(e){
-						console.log('marbles index msg error:', e);
-					}
-				}
-			}
-			
-			//call back for getting a marble, lets send a message
-			function cb_got_marble(e, marble){
-				if(e != null) console.log('marble error:', e);
-				else {
-					try{
-						//wss.broadcast({msg: 'marbles', marble: JSON.parse(marble)});
-					}
-					catch(e){
-						console.log('marble msg error', e);
-					}
-				}
-			}
-			
-			//call back for getting open trades, lets send the trades
-			function cb_got_trades(e, trades){
-				if(e != null) console.log('trade error:', e);
-				else {
-					try{
-						trades = JSON.parse(trades);
-						if(trades && trades.open_trades){
-							//wss.broadcast({msg: 'open_trades', open_trades: trades.open_trades});
-						}
-					}
-					catch(e){
-						console.log('trade msg error', e);
-					}
-				}
-			}
-		});
-	}
-}
