b413d2333e4b6ba28f0385e502dc75c60926e32c
garrettrowe <garrettrowe@yahoo.com>
2017-06-06 17:18:21 -0700

    Update app.js

--- app.js
+++ app.js
@@ -70,6 +70,14 @@ graphD.session(function(err, data) {
   }
 });
 
+graphD.graphs().get(function(err, data){
+  if (err) {
+    console.log("Graph error:" + err);
+  }
+  console.log("Graph data:" + data);
+});
+
+
 //filter for type1 users if we have any
 function prefer_type1_users(user_array){
 	var ret = [];
